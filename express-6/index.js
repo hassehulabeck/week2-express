819852e66a97c8636026dc9169dabd973822262a
hassehulabeck <hansbudandersson@gmail.com>
2020-02-12 09:44:42 +0100

    Added Marcels tip

--- express-6/index.js
+++ express-6/index.js
@@ -4,14 +4,25 @@ const fs = require('fs')
 
 app.use(loggIt)
 
-app.get('/', (req, res) => {
+app.get('/', middlewareOnlyForThisPath, (req, res) => {
     res.write("Hej besökare")
+    res.send()
+})
+
+app.get('*', (req, res) => {
+    res.write("404")
+    res.status(404)
     res.send()
 })
 
 app.listen(3000, () => {
     console.log("Du lyssnar på 3000, de bästa låtarna...")
 })
+
+function middlewareOnlyForThisPath(req, res, next) {
+    console.log("---" + req.path)
+    next()
+}
 
 function loggIt(req, res, next) {
     let loggData = req.hostname + req.path + " " + Date.now() + "\l\r"
